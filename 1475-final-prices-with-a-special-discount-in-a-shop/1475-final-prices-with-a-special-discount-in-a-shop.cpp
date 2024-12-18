4ba98c23d58d37504dbaed26da1deddf1b1713da
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2024-12-18 14:07:49 +0530

    Time: 0 ms (100%), Space: 14.3 MB (9.86%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,12 +2,27 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& p) {
         int n = p.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(p[j]<=p[i]){
-                    p[i]-=p[j];
-                    break;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(p[j]<=p[i]){
+        //             p[i]-=p[j];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return p;
+
+        stack<int> st;
+        st.push(0);
+        for(int i=1;i<n;i++){
+            if(p[i]>p[st.top()])
+                st.push(i);
+            else{
+                while(!st.empty() && p[i]<=p[st.top()]){
+                    p[st.top()]-=p[i];
+                    st.pop();
                 }
+                st.push(i);
             }
         }
         return p;
