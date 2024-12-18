162abbc17c13cd9c78fee06d2e46e030b86911cd
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2024-12-18 10:26:15 +0530

    Time: 0 ms (100%), Space: 14.1 MB (24.39%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& p) {
+        int n = p.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(p[j]<=p[i]){
+                    p[i]-=p[j];
+                    break;
+                }
+            }
+        }
+        return p;
+    }
+};
