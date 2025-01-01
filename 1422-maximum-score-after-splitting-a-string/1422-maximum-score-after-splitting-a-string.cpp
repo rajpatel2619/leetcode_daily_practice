cba096e5916198a0eaba735f8708e4f57cba808d
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-01-01 12:26:58 +0530

    Time: 0 ms (100%), Space: 9.4 MB (11.67%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int ones = 0;
-        int zeros = 0;
-        for(auto c:s)
-            if(c=='1')
-                ones++;
-            else zeros++;
-        int maxi = max(ones-1, zeros-1);
-        int zr = 0;
-        for(int i=0;i<s.size()-1;i++){
-            char ch = s[i];
-            if(ch=='0'){
-                zr++;
-                maxi = max(maxi, ones+zr);
-            }else{
-                ones--;
-            }
+        int n = s.size();
+        vector<int> pref(n), suf(n);
+        pref[0] = (s[0]=='0')?1:0;
+        suf[n-1] = (s[n-1]=='1')?1:0;
+        for(int i=1;i<n;i++){
+            pref[i] = (s[i]=='0')? pref[i-1]+1:pref[i-1];
+        } 
+        for(int i=n-2;i>=0;i--){
+            suf[i] = (s[i]=='1')? suf[i+1]+1:suf[i+1];
+        } 
+        int maxi = max(pref[n-1]-1, suf[0]-1);
+        for(int i=0;i<n-1;i++){
+            if(s[i]=='1') continue;
+            // cout<<pref[i]<<" "<<suf[i]<<endl;
+            maxi = max(maxi, abs(pref[i]+suf[i]));
         }
         return maxi;
     }
