a3525ee6061adb8f754efbb6c9067ef6c7cd8ae8
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-01-04 17:31:40 +0530

    Time: 144 ms (84.05%), Space: 16.1 MB (47.85%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    bool isPal(string &str){
-        return str[0]==str[2];
-    }
-    int func(int ind, string &s, string &curr, unordered_set<string> &st){
-        if(curr.size()==3){
-            if(isPal(curr)){
-                st.insert(curr);
-                return 1;
-            }
-            else return 0;
-        }
-        if(ind>=s.size())
-            return 0;
-        string temp = curr+s[ind];
-        return func(ind+1, s, temp, st) + func(ind+1, s, curr, st);
-    }
     int countPalindromicSubsequence(string s) {
-        unordered_set<string> st;
-        string str = "";
-        func(0, s,str, st);
-        return st.size();
+        unordered_map<char, pair<int, int>> mp;
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            char ch = s[i];
+            if(mp.find(ch)!=mp.end()){
+                mp[ch].second = i;
+            }else{
+                mp[ch].first = mp[ch].second = i;
+            }
+        }
+        // 
+        int count = 0;
+        for(auto [ch, p]:mp){
+            if(p.first==p.second) continue;
+            unordered_set<char> st(s.begin()+p.first+1, s.begin()+p.second);
+            count+=st.size();
+        }
+        return count;
     }
 };
