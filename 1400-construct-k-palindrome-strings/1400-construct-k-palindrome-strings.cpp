d9ab2a4c907a625b30cb3cb6144e64e26e2ed4e9
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-01-11 13:13:52 +0530

    Time: 4 ms (62.59%), Space: 14.8 MB (66.78%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        vector<int> f(26,0);
+        int n = s.size();
+        if(n<k) return false;
+        for(int i=0;i<n;i++)
+            f[s[i]-'a']++;
+        for(auto el:f)
+            cout<<el<<" ";
+        int cnt = 0;
+        for(auto &el:f)
+            if(el&1)
+                cnt++;
+        return cnt<=k;
+    }
+};
