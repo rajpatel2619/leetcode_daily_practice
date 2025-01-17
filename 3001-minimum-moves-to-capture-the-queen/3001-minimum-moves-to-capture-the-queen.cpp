1041533f9cc1bffa4de8db1f5058db1c569917f7
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-01-17 18:37:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (34.93%) - LeetHub

--- 3001-minimum-moves-to-capture-the-queen/3001-minimum-moves-to-capture-the-queen.cpp
+++ 3001-minimum-moves-to-capture-the-queen/3001-minimum-moves-to-capture-the-queen.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
-        // rock..
-        if(a==e){
-            if(a==c){
-                if((d>b && d<f)||(d>f && d<b))
-                    return 2;
-                return 1;
-            }else
-                return 1;
-        }else if(b==f){
-            if(b==d){
-                if((c>a && c<e)||(c>e && c<a))
-                    return 2;
-                return 1;
-            }else 
-                return 1;
+        if(a==e || b==f){
+            if(b==f && b==d && (c-a)*(c-e)<0) return 2;
+            if(a==c && a==e && (d-b)*(d-f)<0) return 2;
+            return 1;
         }
-        // bishobh
         if(abs(c-e)==abs(d-f)){
-            if(abs(c-a)==abs(d-b)){
-                if(abs(c-a)<=abs(c-e) || (abs(d-b)<=abs(d-f))){
-                    if((abs(a-e)+abs(b-f))>= (abs(c-e)+abs(d-f)))
-                        return 1;
-                }else 
-                    return 1;
-            }else
-                return 1;
+            if(abs(c-a) == abs(d-b) && ((b-d)*(b-f)<0)) return 2;
+            return 1;
         }
-        //
         return 2;
     }
 };
