470f1b59f25afe277f6509edad67261f1be67da5
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-01-17 17:14:32 +0530

    Time: 0 ms (100%), Space: 264 MB (48.13%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
@@ -3,9 +3,7 @@ public:
     bool doesValidArrayExist(vector<int>& derived) {
         if(derived.size()==1) return derived[0]==0;
         int xrr = 0;
-        for(auto e:derived)
-            xrr^=e;
-        // xrr^=derived[0];
+        for(auto e:derived) xrr^=e;
         return xrr==0;
     }
 };
