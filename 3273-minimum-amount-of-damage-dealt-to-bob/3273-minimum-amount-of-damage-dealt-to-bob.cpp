fa9e9431977ef46d6369534276698a39d17519d3
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-01-21 20:04:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3273-minimum-amount-of-damage-dealt-to-bob/3273-minimum-amount-of-damage-dealt-to-bob.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long minDamage(int power, vector<int>& damage, vector<int>& health) {
+        int n = damage.size();
+        vector<pair<int, int>> arr(n);
+        for(int i=0;i<n;i++){
+            arr[i] = {damage[i], ceil((double)health[i]/power)};
+        }
+        sort(arr.begin(), arr.end(), [](auto &p, auto &q){
+            return p.first*q.second>=p.second*q.first;
+        });
+
+        long long td = 0, t = 0;
+        for(int i=0;i<n;i++){
+            td+=arr[i].first*(arr[i].second+t);
+            t+=arr[i].second;
+        }
+        return td;
+    }
+};
