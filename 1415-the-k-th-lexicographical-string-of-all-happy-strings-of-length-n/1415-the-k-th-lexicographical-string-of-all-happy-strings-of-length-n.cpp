d342bd88b722104a1dd578423ff49fcbc7e8802a
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-02-19 19:19:10 +0530

    Time: 3 ms (74.09%), Space: 11.5 MB (54.98%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -3,7 +3,7 @@ public:
     bool func(int ind, int n, int &k, string &str, char c){
         if(ind==n) return (!(--k));
         vector<char> arr = {'a','b','c'};
-        for(auto ch:arr){
+        for(auto &ch:arr){
             if(ch!=c){
                 str.push_back(ch);
                 if(func(ind+1, n, k, str, ch)) return true;
