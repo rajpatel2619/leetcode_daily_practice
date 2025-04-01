5aec2fdddeeaf7fda6f68595087147c862e9ce27
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-04-01 21:06:32 +0530

    Time: 106 ms (5.22%), Space: 163.2 MB (5%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    long long func(int ind, vector<vector<int>>& qu,vector<long long>&dp){
-        if(ind>=qu.size()) return 0;
-        
-        if(dp[ind]!=-1) return dp[ind];
-
-        long long take = qu[ind][0] + func(ind+qu[ind][1]+1, qu,dp);
-        long long ntake = 0 + func(ind+1,qu,dp);
-        return dp[ind] = max(take, ntake);
-       
+    unordered_map<int, long long> dp;
+    long long solve(int ind, int n, vector<vector<int>>&arr, long long points){
+        if(ind>=n)
+            return 0;
+        if(dp.find(ind)!=dp.end()) return dp[ind];
+        long long take = arr[ind][0]+solve(ind+arr[ind][1]+1, n, arr, points+arr[ind][0]);
+        long long ntake = solve(ind+1, n, arr, points);
+        return dp[ind] =max(take, ntake);
     }
-    long long mostPoints(vector<vector<int>>& qu) {
-        int n = qu.size();
-        vector<long long> dp(n,-1);
-        return func(0,qu,dp);
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        return solve(0, n, questions, 0);
     }
 };
