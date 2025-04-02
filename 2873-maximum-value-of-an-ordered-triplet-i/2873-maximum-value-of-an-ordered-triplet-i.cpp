a9ec32c52b874b27e4521d8e03d39ab863fabbec
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-04-02 22:09:02 +0530

    Time: 26 ms (6.02%), Space: 22.7 MB (5.12%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
@@ -3,12 +3,21 @@ public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
         long long maxi = 0;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                for(int k=j+1;k<n;k++){
-                    maxi = max(maxi, (long long)nums[k]*(nums[i]-nums[j]));
-                }
-            }
+        vector<int> mx(n), mn(n);
+        mx[0] = nums[0];
+        for(int i=1;i<n;i++){
+            mx[i] = max(mx[i-1], nums[i]);
+        }
+        mn[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            mn[i] = max(mn[i+1], nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            cout<<mx[i]<<" "<<mn[i]<<" n ";
+        }
+        for(int i=1;i<n-1;i++){
+                // cout<<(long long)nums[i]*(mx[j]-mn[j+1])<<endl;
+                maxi = max(maxi, (long long)mn[i+1]*(mx[i-1]-nums[i]));
         }
         return maxi;
     }
