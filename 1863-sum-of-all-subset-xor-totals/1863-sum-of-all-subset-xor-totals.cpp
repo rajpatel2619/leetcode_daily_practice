a35affaf0689c287a1742995a5bc2c1eca7ad16d
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-04-05 22:43:09 +0530

    Time: 6 ms (21.09%), Space: 9.2 MB (71.99%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int ans = 0;
-    void func(int ind, vector<int>& nums, int res){
-        ans+=res;
-        for(int i=ind;i<nums.size();i++){
-            res^=nums[i];
-            func(i+1, nums, res);
-            res^=nums[i];
-        }
-    }
     int subsetXORSum(vector<int>& nums) {
-        func(0, nums, 0);
-        return ans;
+        int n = nums.size();
+        int total_subsets = 1<<n;
+        int res = 0;
+        for(int mask=0;mask<total_subsets;mask++){
+            int xrr = 0;
+            for(int i=0;i<n;i++){
+                if(mask&(1<<i)){
+                    xrr^=nums[i];
+                }
+            }
+            res+=xrr;
+        }
+        return res;
     }
 };
