777fa19304d3ebb177599e9c1e25c04dcc2a9e18
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-05-04 11:39:58 +0530

    Time: 8 ms (35%), Space: 28.5 MB (12.25%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.cpp
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        int n = dominoes.size();
-        unordered_map<int, vector<int>> mp;
+        vector<vector<int>> arr(10,vector<int>(10,0));
         int c = 0;
-        for(int i=0;i<n;i++){
-            vector<int> a = dominoes[i];
-            sort(a.begin(), a.end());
-            if(mp.find(a[0])!=mp.end()){
-                if(mp[a[0]][0]==a[1]){
-                    c+=mp[a[0]][1];
-                    mp[a[0]][1]++;
-                }
-                continue;
-            }
-            mp[a[0]]={a[1],1};
+        for(auto p:dominoes){
+            c+=arr[p[0]][p[1]];
+            arr[p[0]][p[1]]++;
+            if(p[0]!=p[1])
+                arr[p[1]][p[0]]++;
         }
         return c;
     }
