58f61856840aae53387de2472aab794b9ac3681e
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-05-10 11:16:50 +0530

    Time: 0 ms (100%), Space: 8.5 MB (33.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,7 +10,13 @@ public:
         return dp[ind] = one+two;
     }
     int climbStairs(int n) {
+        if(n<2) return n;
         vector<int> dp(n+1, -1);
-        return func(0, n, dp);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
