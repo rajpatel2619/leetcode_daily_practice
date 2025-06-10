1b023dc6a1861e24202763acf2b18f12a06a99bb
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-06-10 11:53:44 +0530

    Time: 4 ms (16.34%), Space: 10 MB (27.18%) - LeetHub

--- 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int maxDifference(string s) {
+        // frequency map [char -> freq]
         unordered_map<int, int> f;
         for(auto ch:s)
             f[ch-'a']++;
+        
+        // greedy way
+        // diff = max_odd_freq - min_even_freq
+        // picking maximum odd freq && minimum even freq
         int odd = INT_MIN, even = INT_MAX;
         for(auto [key, val]:f){
             if(val&1)
                 odd = max(odd, val);
             else even = min(even, val);
         }
+
         return odd-even;
     }
 };
