6d2870dae6cd18a2c703f292f120d1242c6697de
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-06-14 23:34:37 +0530

    Time: 0 ms (100%), Space: 28.2 MB (58.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int el = nums[0];
-        int c=1;
+        int vperson = nums[0];
+        int c = 1;
         for(int i=1;i<n;i++){
-            if(el==nums[i]){
-                c++;
-            continue;
-            }
-            
-            c--;
+            if(nums[i]!=vperson)
+                c--;
+            else c++;
             if(c==0){
-                el=nums[i];
+                vperson = nums[i];
                 c=1;
             }
         }
-        return el;
+        return vperson;
     }
 };
