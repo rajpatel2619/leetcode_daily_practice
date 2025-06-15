da70d8e8f55fe711819f970e8d24d2044fc05315
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-06-15 10:49:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxDiff(int num) {
+        string s = to_string(num);
+        string maxi = s, mini = s;
+
+        //modifyng maxi for the max value
+        int i = 0;
+        while(s[i]=='9')
+            i++;
+        if(i<s.size()){
+            char ch = s[i];
+            for(i;i<s.size();i++){
+                if(s[i]==ch)
+                    maxi[i] = '9';
+            }
+        }
+
+        //modifying mini for the min value
+        if(s[0]=='1'){
+            i = 1;
+            while(s[i]=='1')
+                i++;
+            char ch = s[i];
+            for(i;i<s.size();i++){
+                if(s[i]==ch)
+                    mini[i] = '0';
+            }
+        }else{
+            i = 0;
+            char ch = s[i];
+            for(i;i<s.size();i++){
+                if(s[i]==ch)
+                    mini[i] = '1';
+            }
+        }
+        cout<<maxi<<" "<<mini;
+        return stoi(maxi) - stoi(mini);
+    }
+};
