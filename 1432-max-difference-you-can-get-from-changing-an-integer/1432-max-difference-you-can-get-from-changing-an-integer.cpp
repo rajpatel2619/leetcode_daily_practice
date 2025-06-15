1691efbc0cb857b497019cf5693b371773c47b41
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-06-15 11:08:47 +0530

    Time: 0 ms (100%), Space: 8 MB (93.84%) - LeetHub

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -6,35 +6,18 @@ public:
 
         //modifyng maxi for the max value
         int i = 0;
-        while(s[i]=='9')
-            i++;
-        if(i<s.size()){
-            char ch = s[i];
-            for(i;i<s.size();i++){
-                if(s[i]==ch)
-                    maxi[i] = '9';
-            }
-        }
+        while(s[i]=='9') i++;
+        replace(maxi.begin()+i, maxi.end(), s[i], '9');
 
         //modifying mini for the min value
         if(s[0]=='1'){
             i = 1;
-            while(s[i]=='1' or s[i]=='0')
-                i++;
-            char ch = s[i];
-            for(i;i<s.size();i++){
-                if(s[i]==ch)
-                    mini[i] = '0';
-            }
+            while(s[i]=='1' or s[i]=='0') i++;
+            replace(mini.begin()+i, mini.end(), s[i], '0');
         }else{
-            i = 0;
-            char ch = s[i];
-            for(i;i<s.size();i++){
-                if(s[i]==ch)
-                    mini[i] = '1';
-            }
+            replace(mini.begin(), mini.end(), s[0], '1');
         }
-        cout<<maxi<<" "<<mini;
+        // max diff
         return stoi(maxi) - stoi(mini);
     }
 };
