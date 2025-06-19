fdd9b4c99b2273c5c787586d5229dbca047b4373
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-06-19 07:51:29 +0530

    Time: 31 ms (81.44%), Space: 86.7 MB (96.08%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.cpp
@@ -23,21 +23,16 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         // [1, 1, 3, 3, 4, 5, 7, 8, 9], k = 2, (size - n/3 = 3)
-        vector<int> temp;
+        
         int mini = nums[0];
-        temp.push_back(mini);
-        vector<vector<int>> res;
+        int count = 0;
         for(int i=1;i<n;i++){
-            if((nums[i]-mini)<=k){
-                temp.push_back(nums[i]);
-            }else{
-                res.push_back(temp);
-                temp.clear();
+            if((nums[i]-mini)>k){
+                count++;
                 mini = nums[i];
-                temp.push_back(mini);
             }
         }
-        res.push_back(temp);
-        return res.size();
+        count++;
+        return count;
     }
 };
