2520ef6d5c2a2d42a5c129d157b8d5f74d99bc62
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-10-22 14:08:15 +0530

    Time: 27 ms (7.42%), Space: 10.9 MB (39.98%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int count(string &str, int k, char ch){
-        int l=0, r=0, n = str.size(), cnt = 0, maxi = 0;
+    int func(char ch, string &str, int k){
+        int l = 0, r = 0, n = str.size(), count = 0, maxi = 1;
         while(r<n){
-            char c = str[r++];
-            if(c!=ch){
-                cnt++;
-                if(cnt>k){
-                    while(l<r and cnt>k){
-                        if(str[l++]!=ch)cnt--;
-                    }
-                }
-            }
+            if(str[r]==ch) r++;
+            else count++, r++;
+
+            while(l<r and count>k)
+                if(str[l++]!=ch) count--;
+                
             maxi = max(maxi, r-l);
         }
         return maxi;
     }
     int characterReplacement(string s, int k) {
         int maxi = 0;
-        for(char ch='A';ch<='Z';ch++){
-            maxi = max(maxi, count(s, k, ch));
-        }
+        for(char ch = 'A';ch<='Z';ch++)
+            maxi = max(maxi, func(ch, s, k));
         return maxi;
     }
 };
