784704d3db53386454c9e0ce8637999d4717abd7
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-10-26 19:39:21 +0530

    Time: 47 ms (34.97%), Space: 48.9 MB (40.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -12,11 +12,23 @@ public:
         }
         return dp[ind][tar] = ans;
     }
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = func(n-1, coins, amount, dp);
-        if(ans>=1e8) return -1;
-        return ans;
+    int coinChange(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(target+1, 0));
+        // base case
+        for(int j=0;j<=target;++j){
+            if(j%nums[0]==0) dp[0][j] = j/nums[0];
+            else dp[0][j] = 1e8;
+        }
+        // main code
+        for(int i=1;i<n;i++){
+            for(int j = 1;j<=target;++j){
+                int ans = dp[i-1][j];
+                if(nums[i]<=j)
+                    ans = min(ans, 1+dp[i][j-nums[i]]);
+                dp[i][j] = ans;
+            }
+        }
+        return dp[n-1][target]>=1e8?-1:dp[n-1][target];
     }
 };
