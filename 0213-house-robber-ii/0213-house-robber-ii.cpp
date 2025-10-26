d9ea1da4736e81bbbb15cab91ab1bc220d6c4514
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-10-26 19:06:36 +0530

    Time: 0 ms (100%), Space: 10.9 MB (20.32%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int func(int ind, int prev, vector<int>& nums, vector<vector<int>> &dp){
-        int n = nums.size();
-        if(ind==n) return 0;
-        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-        int take = 0;
-        if(prev==-1 || ind!=prev+1){
-            take = nums[ind] + func(ind+1, ind, nums, dp);
-        }
-        int ntake = func(ind+1, prev, nums, dp);
-        return dp[ind][prev+1] = max(take, ntake);
-    }
-    int rob2(vector<int>& nums){
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return func(0, -1, nums, dp);
-    }
+    int rob1(vector<int>& nums, int s, int e) {
 
+        vector<int> dp(e-s+1, 0);
+
+        // base case
+        dp[s] = nums[s];
+        int p = nums[s], sp = 0;
+        for(int i=s+1;i<e;i++){
+            int ntake = p;
+            int take = nums[i];
+            if(i-2>=0)
+                take += sp;
+            sp = p;
+            p =  max(take, ntake);
+        }
+        return p;
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
-        vector<int> temp1, temp2;
-        for(int i=0;i<n;i++){
-            if(i!=0)temp1.push_back(nums[i]);
-            if(i!=n-1) temp2.push_back(nums[i]);
-        }
-        return max(rob2(temp1), rob2(temp2));
+        return max(rob1(nums, 0, n-1), rob1(nums, 1, n));
     }
 };
