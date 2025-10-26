9eb496eb19d68e73db9d1420063b644fa0c6bff2
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-10-26 12:10:33 +0530

    Time: 4 ms (40.98%), Space: 18 MB (25.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void func(int ind, vector<int>& nums, vector<int> &path, vector<vector<int>>&res, int target){
-        
-            if(target == 0){
+    void func(int ind, vector<int> &nums, int tar, vector<int>&path, vector<vector<int>> &res){
+        if(ind==-1){
+            if(tar==0)
                 res.push_back(path);
-            return;}
-        
-        if(target<0) return;
-        for(int i=ind;i<nums.size();i++){
-            path.push_back(nums[i]);
-            func(i, nums, path, res, target-nums[i]);
+            return;
+        }
+        func(ind-1, nums, tar, path, res);
+        if(nums[ind]<=tar){
+            path.push_back(nums[ind]);
+            func(ind, nums, tar-nums[ind], path, res);
             path.pop_back();
         }
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& nums, int tar) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> res;
-        vector<int>path;
-        func(0, candidates, path, res, target);
+        vector<int> path;
+        func(nums.size()-1, nums, tar, path, res);
         return res;
     }
 };
